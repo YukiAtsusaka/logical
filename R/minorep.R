baa74dcfff3366f637f51554316a059f1e90b22a
Yuki Atsusaka <45901291+YukiAtsusaka@users.noreply.github.com>
2021-05-20 15:43:25 -0500

    Update minorep.R

--- R/minorep.R
+++ R/minorep.R
@@ -4,21 +4,27 @@
 #'
 #' @param M a vector of raw margin of victories
 #' @param C a vector of the percentages of minority voters in districts
-#' @param sd the pre-specified value of the standard deviation in the standard normal CDF
+#' @param gap a vector of the turnout rates of minority and White voters
 #'
 #' @return A list containing main results ($Results) and related statistics ($Stats).
 #' @examples
-#' rmargin = c(20, 50, 30)
-#' percent = c(40, 70, 85)
-#' emerge(M=rmargin, C=percent)
-#' emerge(M=rmargin, C=percent, sd=5)
+#' M_vec = c(20, 50, 30)
+#' C_vec = c(40, 70, 85)
+#' minorep(M=M_vec, C=C_vec) # Assuming no turnout gap
+#' minorep(M=M_vec, C=C_vec, gap=c(0.5, 0.6)) # Assuming that minority turnout is 0.5 and White turnout is 0.6 
 #' @export
 
-minorep <- function(M, C, sd=1){
+minorep <- function(M, C, gap=NULL){
 
-sd=sd
-q = sqrt((M+50)*C) - 50 # GEOMETRIC MEAN
-p = pnorm(q=q,  mean=0, sd=sd)
+# Computing Turnout Adjusted Percentage of Minority Voters
+if(is.null(gap)){
+C = C
+}else{
+C = (C*gap[1])/(C*gap[1] + (100-C)*gap[2])*100
+}
+
+q = sqrt(M*C) - 50             # Geometric mean of the two bounds
+p = pnorm(q=q,  mean=0, sd=1)  # Model prediction
   
 return(p)
 }
